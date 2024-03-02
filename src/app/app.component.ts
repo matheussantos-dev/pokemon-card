729beb2f77c7dc11a6df915a318fa6d18538a0f1
Matheus dos Santos da Silva <matheussantos4@icloud.com>
2024-03-02 15:22:51 -0300

    add: home page styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { DeckService } from './services/deck.service';
 
 
 @Component({
@@ -7,11 +6,4 @@ import { DeckService } from './services/deck.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  constructor(private deckService: DeckService) {}
-
-  logDecks() {
-    const decks = this.deckService.getDecks();
-    console.log(decks);
-  }
-}
+export class AppComponent {}
